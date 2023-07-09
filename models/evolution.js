eb714b8fd3fe866df358ced21e0178de90cdea5f
DCaldwell13 <draykcaldwell0516@gmail.com>
2023-07-09 13:00:13 -0700

    updated

--- models/evolution.js
+++
@@ -1,2 +0,0 @@
-const { Model } = require('sequelize');
-
--- pokedex/models/evolution.js
+++ pokedex/models/evolution.js
@@ -3,7 +3,26 @@ const { Model } = require ('sequelize');
 module.exports = (sequelize,DataType) => {
     class Evolution extends Model {
         static associate({ pokemon }) {
-            
+            Evolution .hasMany(pokemon, {
+                foreignKey: 'evolution_id',
+                as:'evolution'
+            })
         }
     }
+    Evolution.init({
+        evolution_id: {
+            type: DataType.string,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        element_id: {
+            type: DataType.string,
+            allowNull: false
+        }, {
+            sequelize, 
+            modelName: 'evolution',
+            tableName: 'evolution_id',
+            timestamps:false
+    })
+    return Evolution;
 }
