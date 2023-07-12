bf565e887154b245fb26be1d8e38bf12a780f1a5
DCaldwell13 <draykcaldwell0516@gmail.com>
2023-07-11 20:23:51 -0700

    updated code

--- pokedex/server.js
+++ pokedex/server.js
@@ -1,17 +1,18 @@
+//DEPENDENCIES
 const express = require('express')
 const {Sequelize } = require('sequelize')
 const app = express()
-
+//CONFIGURATION / NIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use (express.urlencoded({ extend: false }))
-
+//ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message:'Welcome to the Paldea PokeDex'
     })
 })
-
+//LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${ process.env.PORT }`)
 })
